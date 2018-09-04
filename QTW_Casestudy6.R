56e15c7e306ae2446f6ff1c82f753f72f4194f85
rmandava6 <rmandava@smu.edu>
2018-09-04 16:29:07 -0500

    First Edit
    Added data files and R code for Q1 on Casestudy6

---
+++ QTW_Casestudy6.R
@@ -0,0 +1,33 @@
+library(plyr)
+library(sqldf)
+
+data <- read.csv('/Users/ramya/Documents/SMU/QTW/Week1/CaseStudy6_2/docword.nips.txt',sep = ' ', skip=3, header=F)
+colnames(data) <- c("doc","word", "count")
+head(data, n=10)
+
+#Creating column tf and calculating the value for tf as ln(1+count)
+data2 <- data
+data2$tf <- log(1+data2$count)
+
+#Calculate n value --> number of times a word appeared in all documents(across docs)
+data3 <-data
+nCount <- count(data3, "word")
+head(ncount, n=10)
+
+#Calculate idf for each word and assign it to new data frame 'idfData'
+nCount$idf <- log(1 + (N/nCount$freq))
+head(nCount, n=10)
+idfData <- nCount
+head(idfData, n=10)
+
+tfidfdata <- merge(x = data2, y = idfData, by = "word", all = TRUE)
+head(tfidfdata, n=10)
+
+#Calculate tf-idf scores per word per doc
+tfidfdata$tfIdf <- tfidfdata$tf*tfidfdata$idf
+head(tfidfdata,n=10)
+
+#Assign final matrix to tfidfData
+tfidfData <- tfidfdata[c(1,2,4,6,7)]
+head(tfidfData, n=10)
+
