8e87ae16af151dd1e56257e1cec17425815c3b7b
rmandava6 <rmandava@smu.edu>
2018-09-08 17:56:02 -0500

    Dbscan
    DBscan code added

--- QTW_Casestudy6.R
+++ QTW_Casestudy6.R
@@ -1,5 +1,7 @@
 library(plyr)
 library(sqldf)
+library(cluster) 
+library(dbscan)
 
 N <- 1500
 
@@ -8,12 +10,11 @@ colnames(data) <- c("doc","word", "count")
 head(data, n=10)
 
 #Creating column tf and calculating the value for tf as ln(1+count)
-data2 <- data
-data2$tf <- log(1+data2$count)
+data$tf <- log(1+data2$count)
+head(data, n=10)
 
 #Calculate n value --> number of times a word appeared in all documents(across docs)
-data3 <-data
-nCount <- count(data3, "word")
+nCount <- count(data, "word")
 head(nCount, n=10)
 
 #Calculate idf for each word and assign it to new data frame 'idfData'
@@ -36,6 +37,7 @@ head(tfidfData, n=10)
 tfidfDatafinal <- tfidfData[c(1,5)]
 head(tfidfDatafinal, n=10)
 
+#kmeans with 5 clusters
 set.seed(20)
 clusters <- kmeans(tfidfDatafinal, 5)
 
@@ -44,17 +46,19 @@ tfidfDatafinal$kcluster <- as.factor(clusters$cluster)
 head(tfidfDatafinal, n=10)
 
 tfidfDatafinal2 <- tfidfDatafinal[c(1,2)]
+head(tfidfDatafinal2, n=10)
 str(clusters)
 
-#Used
-library(cluster)
-library(fpc)
-
-# Fig 01
-plotcluster(tfidfDatafinal[100, c(1,3)], clusters$cluster)
-
 # vary parameters for most readable graph
-library(cluster) 
+
+clusplot(tfidfDatafinal2, clusters$cluster, color=TRUE, shade=TRUE, 
+         labels=2, lines=0)
+
+#kmeans with 10 clusters
+set.seed(10)
+clusters1 <- kmeans(tfidfDatafinal, 5)
+tfidfDatafinal2 <- tfidfDatafinal[c(1,2)]
+head(tfidfDatafinal2, n=10)
 clusplot(tfidfDatafinal2, clusters$cluster, color=TRUE, shade=TRUE, 
          labels=2, lines=0)
 
@@ -68,3 +72,21 @@ dE2   <- dissE^2
 sk2   <- silhouette(km$cl, dE2)
 plot(sk2)
 
+#Running with test data
+tfidfTestdata <- tfidfDatafinal[c(1,2)]
+tail(tfidfTestdata, n=10)
+
+#DBSCAN
+## find suitable eps parameter using a k-NN plot for k = dim + 1
+## Look for the knee!
+#References: https://en.proft.me/2017/02/3/density-based-clustering-r/
+#https://www.rdocumentation.org/packages/dbscan/versions/1.1-2/topics/dbscan
+kNNdistplot(tfidfTestdata, k = 3)
+abline(h=.5, col = "red", lty=2)
+
+set.seed(1234)
+res <- dbscan(tfidfTestdata, eps = 3, minPts = 100000)
+hullplot(tfidfTestdata, res$cluster)
+
+max(res$cluster)
+pairs(iris, col = res$cluster + 1L)
