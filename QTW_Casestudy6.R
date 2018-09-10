c5c2201abd4ea9ee6efdf4519306a2cddcc84156
rmandava6 <rmandava@smu.edu>
2018-09-09 19:06:24 -0500

    Word Cloud Code

--- QTW_Casestudy6.R
+++ QTW_Casestudy6.R
@@ -31,10 +31,10 @@ tfidfdata$tfIdf <- tfidfdata$tf*tfidfdata$idf
 head(tfidfdata,n=10)
 
 #Assign final matrix to tfidfData
-tfidfData <- tfidfdata[c(1,2,4,6,7)]
-head(tfidfData, n=10)
+tfidfData2 <- tfidfdata[c(1,2,4,6,7)]
+head(tfidfData2, n=10)
 
-tfidfDatafinal <- tfidfData[c(1,5)]
+tfidfDatafinal <- tfidfData2[c(1,5)]
 head(tfidfDatafinal, n=10)
 
 #kmeans with 5 clusters
@@ -85,8 +85,86 @@ kNNdistplot(tfidfTestdata, k = 3)
 abline(h=.5, col = "red", lty=2)
 
 set.seed(1234)
-res <- dbscan(tfidfTestdata, eps = 3, minPts = 100000)
+res <- dbscan(tfidfTestdata, eps = 3, minPts = 3)
 hullplot(tfidfTestdata, res$cluster)
 
 max(res$cluster)
 pairs(iris, col = res$cluster + 1L)
+
+
+#Creating histogram
+
+hist(tfidfdata$count, 
+     main="Histogram for words", 
+     xlab="Count", 
+     xlim=c(0,135),
+     border="blue", 
+     col="green",
+     breaks=50)
+
+hist(tfidfdata$tfIdf, 
+     main="Histogram for words", 
+     xlab="Count", 
+     xlim=c(0,33),
+     border="blue", 
+     col="green",
+     breaks=20)
+
+#word cloud testing 
+install.packages("tm")  # for text mining
+install.packages("SnowballC") # for text stemming
+install.packages("wordcloud") # word-cloud generator 
+install.packages("RColorBrewer") # color palett
+
+library("tm")
+library("SnowballC")
+library("wordcloud")
+library("RColorBrewer")
+
+dataTxt <- read.csv('/Users/ramya/Documents/SMU/QTW/Week1/CaseStudy6_2/vocab.nips.txt',header=F)
+colnames(dataTxt) <- c("wordtext")
+head(dataTxt, n=10)
+
+dataTxt2 <-dataTxt
+
+tfidfdata
+inspect(dataTxt)
+
+dataTxt2$word <- seq.int(from=1, to=12419, by=1)
+tfidfdata3<-tfidfdata
+dataTxtFinal <- merge(x = tfidfdata3, y = dataTxt2, by = "word", all = FALSE)
+
+head(dataTxtFinal, n=10)
+
+clouddata <- dataTxtFinal[dataTxtFinal$doc == 1,]
+
+hist(clouddata$freq, 
+     main="Histogram for words", 
+     xlab="Words", 
+     border="blue", 
+     col="green",
+     breaks=20)
+
+set.seed(1234)
+wordcloud(words = clouddata$wordtext, freq = clouddata$freq, min.freq = 110,
+          max.words=150, random.order=FALSE, rot.per=0.35, 
+          colors=brewer.pal(8, "Dark2"))
+
+
+#word cloud on kmeans 10
+set.seed(10)
+clusters1 <- kmeans(tfidfDatafinal, 10)
+tfidfDatafinal4 <- tfidfDatafinal
+
+
+tfidfDatafinal4$kcluster <- as.factor(clusters1$cluster)
+head(tfidfDatafinal4, n=10)
+
+tfidfDatafinal4<-tfidfDatafinal4[c(1,3)]
+head(tfidfDatafinal4, n=10)
+
+tfidfDatafinal4 <- merge(x = tfidfdata, y = tfidfDatafinal4, by = "word", all = FALSE)
+
+#dataTxt2
+tfidfdata3<-tfidfdata
+dataTxtFinal <- merge(x = tfidfdata3, y = dataTxt2, by = "word", all = FALSE)
