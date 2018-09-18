d1089d374eb2475a7f03dc0747dd71331d9d6ff7
rmandava6 <rmandava@smu.edu>
2018-09-17 22:16:02 -0500

    Week2 CS8 draft 1 added

---
+++ Week2/CS8_QTW_Draft1.R
@@ -0,0 +1,84 @@
+library(XML)
+ubase = "http://www.cherryblossom.org/"
+womenURLs = 
+  c("results/1999/cb99f.html", "results/2000/Cb003f.htm", "results/2001/oof_f.html",
+    "results/2002/ooff.htm", "results/2003/CB03-F.HTM",
+    "results/2004/women.htm", "results/2005/CB05-F.htm", 
+    "results/2006/women.htm", "results/2007/women.htm", 
+    "results/2008/women.htm", "results/2009/09cucb-F.htm",
+    "results/2010/2010cucb10m-f.htm", 
+    "results/2011/2011cucb10m-f.htm",
+    "results/2012/2012cucb10m-f.htm")
+
+urls = paste(ubase, womenURLs, sep = "")
+
+urls[1:3]
+
+
+urls = paste(ubase, womenURLs, sep = "")
+
+urls[1:3]
+
+
+
+extractResTablew =
+  # takes a list of websites from the cherry blossom race
+  # a list of years corresponding to the year the result is for
+  # and the gender of the participant
+  # Retrieve data from web site, 
+  # find the preformatted text,
+  # and write lines or return as a character vector.
+  # returns a list of strings corrsponding to lines in the web url
+  function(url = "http://www.cherryblossom.org/results/2009/09cucb-F.htm",
+           year = 1999, sex = "female", file = NULL)
+  {
+    doc = htmlParse(url)
+    
+    if (year == 2000) {
+      # Get preformatted text from 4th font element
+      # The top file is ill formed so the <pre> search doesn't work.
+      ff = getNodeSet(doc, "//font")
+      txt = xmlValue(ff[[4]])
+      els = strsplit(txt, "\r\n")[[1]]
+    }
+    else if (year == 2009 & sex == "female") {
+      # Get preformatted text from <div class="Section1"> element
+      # Each line of results is in a <pre> element
+      div1 = getNodeSet(doc, "//div[@class='Section1']")
+      pres = getNodeSet(div1[[1]], "//pre")
+      els = sapply(pres, xmlValue)
+    }
+    else if (year == 1999) {
+      # Get preformatted text from <pre> elements
+      pres = getNodeSet(doc, "//pre")
+      txt = xmlValue(pres[[1]])
+      els = strsplit(txt, "\n")[[1]]   
+    } 
+    else {
+      # Get preformatted text from <pre> elements
+      pres = getNodeSet(doc, "//pre")
+      txt = xmlValue(pres[[1]])
+      els = strsplit(txt, "\r\n")[[1]]   
+    } 
+    
+    if (is.null(file)) return(els)
+      # Write the lines as a text file.
+      writeLines(els, con = file)
+  }
+
+
+years = 1999:2012
+womenTables = mapply(extractResTable, url = urls, year = years)
+names(womenTables) = years
+sapply(menTables, length)
+
+#womenTables = mapply(extractResTable, url = urls, 
+#                       year = years, sex = rep("female", 14))
+#names(womenTables) = years
+#sapply(womenTables, length)
+
+
+save(menTables, file = "CBMenTextTables.rda")
+
+To access records:
+  menTables$’2009’
